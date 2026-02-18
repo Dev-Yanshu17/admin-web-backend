85d687d1d6c9f084464a984acc1994424944f0ea
yanshu <bhingaradiyayanshu@gmail.com>
2026-02-18 17:28:40 +0530

    added

--- routes/contact.js
+++ routes/contact.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const Contact = require("../models/Contact");
 
 router.get("/", async (req, res) => {
-  try {
-    const contacts = await Contact.find(); // Fetch all contacts from DB
-    res.json(contacts);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch contacts" });
-  }
+  const contacts = await Contact.find().sort({ createdAt: -1 });
+  res.json(contacts);
 });
+
 
 router.post("/", async (req, res) => {
   try {
